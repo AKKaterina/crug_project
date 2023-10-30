5102b91a22b07a146930bf339d1ce4ed10f612fd
ekaterinaagureeva <ekaterina.agureeva@thirdbrain.ch>
2023-10-30 10:27:58 +0200

    Added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Registration } from "./pages/Registration";
+import { UserManagementList } from "./pages/UserManagementList";
+import { Error } from "./pages/Error";
+import "./App.css";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <div>Hello world!</div>,
-    },
+	{
+		path: "/login",
+		element: <Login />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/registration",
+		element: <Registration />,
+        errorElement: <Error />,
+	},
+	{
+		path: "/",
+		element: <UserManagementList />,
+        errorElement: <Error />,
+	},
 ]);
 
 function App() {
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
